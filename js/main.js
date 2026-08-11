dd7b2b9bc009349a3d8e3456c4c989e5ca44f47a
ordakir <73047570+ordakir@users.noreply.github.com>
2026-08-10 23:12:41 -0600

    Add navigation toggle functionality

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const navToggle = document.querySelector(".nav-toggle");
+const navMenu = document.querySelector(".nav-menu");
+
+if (navToggle && navMenu) {
+  navToggle.addEventListener("click", () => {
+    const isOpen = navMenu.classList.toggle("is-open");
+
+    navToggle.setAttribute(
+      "aria-expanded",
+      isOpen
+      );
+  });
+}
