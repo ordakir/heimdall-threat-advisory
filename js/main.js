de95bfba95833aa20bfbaac845251de6ff7d7770
ordakir <73047570+ordakir@users.noreply.github.com>
2026-08-12 14:28:20 -0600

    Enhance navigation menu with link click behavior
    Added functionality to close the navigation menu when a link is clicked.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 const navToggle = document.querySelector(".nav-toggle");
 const navMenu = document.querySelector(".nav-menu");
+const navLinks = document.querySelectorAll(".nav-menu a");
 
 if (navToggle && navMenu) {
   navToggle.addEventListener("click", () => {
@@ -10,4 +11,16 @@ if (navToggle && navMenu) {
       isOpen
       );
   });
+
+  navLinks.forEach(link) => {
+  link.addEventListener("click", () => {
+    navMenu.clasList.remove("is-open");
+
+    navToggle.setAttribute(
+      "aria-expanded",
+      "false"
+      );
+  });
+  });
+  
 }
